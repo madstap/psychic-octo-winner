065d5288115c5f8beb8dbe660a88c3673959f98d
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2023-03-13 17:18:35 -0300

    Add flush to log to make it nicer to follow along in real time

--- src/kitchen/log.clj
+++ src/kitchen/log.clj
@@ -2,4 +2,5 @@
 
 (defn stdout-log [event data]
   ;; Pass a single string arg to avoid interleaving concurrent prints
-  (print (str event " " (pr-str data) "\n")))
+  (print (str event " " (pr-str data) "\n"))
+  (flush))
